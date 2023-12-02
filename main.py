48da0a6b207db4879856e165a17778f5ee24bd86
DeltaHeavyVIP <patutin.vovan@mail.ru>
2023-12-02 22:56:07 +0300

    aaaaaaaa

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ df['Время заваривания (секунд)'] = df['Время зава
 tastes = np.array(df['Вкус'])
 tastes
 
+
 # функция, которая создает дерево вкусов
 def create_node(extra_node, object):
     extra = object.split('; ')
@@ -26,6 +27,7 @@ def create_node(extra_node, object):
             extra_node.addkid(next_node)
         else:
             extra_node.addkid(Node(item))
+
 
 # Древесная мера близости по основному вкусовому оттенку
 objects = tastes
@@ -46,6 +48,7 @@ class TeaCat:
         self.children = []
         if par is not None:
             par.children.append(self)
+
 
 catRoot = TeaCat('root', None)
 catSublim = TeaCat('Растворимый', catRoot)
@@ -100,16 +103,18 @@ def metricTree(a, b):
 
     return (cntr + anc1[cur.name]) / 4.0
 
+
 objects = np.array(df['Категория'])
 matrix_elem_kat = np.zeros((objects.shape[0], objects.shape[0]))
 for i in range(objects.shape[0]):
     for j in range(objects.shape[0]):
-        matrix_elem_kat[i, j] = metricTree(objects[i],objects[j])
+        matrix_elem_kat[i, j] = metricTree(objects[i], objects[j])
 
 
 # Функция для расчета евклидовой меры близости между объектами по времени заваривания
 def similarity_measure(obj1, obj2):
     return np.linalg.norm(obj1 - obj2)
+
 
 # получаем массив объектов по времени заваривания
 objects = np.array(df['Время заваривания (секунд)'])
@@ -124,11 +129,11 @@ for i in range(objects.shape[0]):
 objects = np.array(df['Температура заваривания C'])
 matrix_evklid_temp = np.zeros((objects.shape[0], objects.shape[0]))
 
-
 # Расчет значений меры для каждой пары объектов
 for i in range(objects.shape[0]):
     for j in range(objects.shape[0]):
         matrix_evklid_temp[i, j] = similarity_measure(objects[i], objects[j])
+
 
 # Функция для расчета меры близости по коэффициенту Жаккара
 def jaccard_similarity_measure(obj1, obj2):
@@ -139,13 +144,13 @@ def jaccard_similarity_measure(obj1, obj2):
     else:
         return (intersection / union)
 
+
 # получаем массив объектов по признаку веганский или нет (бинарная ассоциативная)
 objects = np.array(df['Есть ароматизатор'].replace(to_replace=['False', 'True'], value=['0', '1'])).astype(int)
 matrix_binary = np.zeros((objects.shape[0], objects.shape[0]))
 for i in range(objects.shape[0]):
     for j in range(objects.shape[0]):
         matrix_binary[i, j] = jaccard_similarity_measure(objects[i], objects[j])
-
 
 # создание общей меры близости
 recomender_matrix = matrix_tree_main + matrix_elem_kat + matrix_evklid_temp * 0.01 + matrix_evklid_time * 0.01 + matrix_binary * 2
@@ -154,6 +159,7 @@ recomender_matrix = matrix_tree_main + matrix_elem_kat + matrix_evklid_temp * 0.
 plt.imshow(recomender_matrix, cmap='hot', interpolation='nearest')
 plt.colorbar()
 plt.show()
+
 
 def get_recomendation(likes, dislikes, data):
     def find(name):
@@ -185,11 +191,7 @@ def get_recomendation(likes, dislikes, data):
     return result
 
 
-result = get_recomendation(['Шайцинь Юцзи Шэн', 'Путь Сердца'], ['Юцзи Гаоцзи Шу Ч'], df)
-result
-
-def do_fliter(recomendation, name=None, category=None, form=None, is_arom=None, temp=[None, None], time=[None, None],
-              price=None, taste=None):
+def do_fliter(recomendation, filterEntity):
     # from expert_options.csv
     term_to_price = {
         'Очень дешевый': lambda x: int(x) <= 1000,
@@ -199,33 +201,127 @@ def do_fliter(recomendation, name=None, category=None, form=None, is_arom=None, 
         'Очень дорогой': lambda x: int(x) > 4750,
     }
 
-    if name is not None:
-        recomendation = recomendation[recomendation['Название'].apply(lambda x: x.find(name) != -1)]
+    if filterEntity.name is not None:
+        recomendation = recomendation[recomendation['Название'].apply(lambda x: x.find(filterEntity.name) != -1)]
 
-    if category is not None:
-        recomendation = recomendation[recomendation['Категория'].apply(lambda x: x == category)]
+    if filterEntity.category is not None:
+        recomendation = recomendation[recomendation['Категория'].apply(lambda x: x == filterEntity.category)]
 
-    if form is not None:
-        recomendation = recomendation[recomendation['Форма упаковки'].apply(lambda x: x == form)]
+    if filterEntity.form is not None:
+        recomendation = recomendation[recomendation['Форма упаковки'].apply(lambda x: x == filterEntity.form)]
 
-    if is_arom is not None:
-        recomendation = recomendation[recomendation['Есть ароматизатор'].apply(lambda x: x == is_arom)]
+    if filterEntity.is_arom is not None:
+        recomendation = recomendation[recomendation['Есть ароматизатор'].apply(lambda x: x == filterEntity.is_arom)]
 
-    if temp[0] is not None and temp[1] is not None:
+    if filterEntity.temp[0] is not None and filterEntity.temp[1] is not None:
         recomendation = recomendation[
-            recomendation['Температура заваривания C'].apply(lambda x: temp[0] < int(x) <= temp[1])]
+            recomendation['Температура заваривания C'].apply(
+                lambda x: filterEntity.temp[0] < int(x) <= filterEntity.temp[1])]
 
-    if time[0] is not None and time[1] is not None:
+    if filterEntity.time[0] is not None and filterEntity.time[1] is not None:
         recomendation = recomendation[
-            recomendation['Время заваривания (секунд)'].apply(lambda x: time[0] < int(x) <= time[1])]
+            recomendation['Время заваривания (секунд)'].apply(
+                lambda x: filterEntity.time[0] < int(x) <= filterEntity.time[1])]
 
-    if taste is not None:
-        recomendation = recomendation[recomendation['Вкус'].apply(lambda x: x.find(name) != -1)]
+    if filterEntity.taste is not None:
+        recomendation = recomendation[recomendation['Вкус'].apply(lambda x: x.find(filterEntity.name) != -1)]
 
-    if price is not None:
-        recomendation = recomendation[recomendation['стоимость'].apply(term_to_price[price])]
+    if filterEntity.price is not None:
+        recomendation = recomendation[recomendation['стоимость'].apply(term_to_price[filterEntity.price])]
 
     return recomendation
 
-result = do_fliter(result, name='а', is_arom=True, temp=[0, 100])
-result
+
+class FilterEntity:
+    name = None
+    category = None
+    form = None
+    is_arom = None
+    temp = [None, None]
+    time = [None, None]
+    price = None
+    taste = None
+
+
+def main_loop(likes, dislikes, filterEntity) -> int:
+    try:
+        print(
+            "0 - Поставить лайк\n" +
+            "1 - Поставить дизлайк\n" +
+            "2 - Установить фильтр\n" +
+            "3 - Получить рекомендацию\n" +
+            "4 - Выход"
+        )
+        print("\nНапишите индекс меню")
+        menu_entry_index = int(input())
+        if menu_entry_index == 0:
+            print("\nНапишите название чая")
+            name = input().strip()
+            if not (name in likes):
+                likes.append(name)
+            return 0
+        elif menu_entry_index == 1:
+            print("\nНапишите название чая")
+            name = input().strip()
+            if not (name in dislikes):
+                dislikes.append(name)
+            return 0
+        elif menu_entry_index == 2:
+            print("\nНапишите параметр для фильтрации")
+            key = input()
+            print("\nНапишите значение для фильтрации")
+            if key == 'name':
+                value = input()
+                filterEntity.name = value
+            elif key == 'category':
+                value = input()
+                filterEntity.category = value
+            elif key == 'form':
+                value = input()
+                filterEntity.form = value
+            elif key == 'is_arom':
+                value = bool(input())
+                filterEntity.is_arom = value
+            elif key == 'temp':
+                print("\nНапишите дополнительное значение для фильтрации")
+                value1 = int(input())
+                value2 = int(input())
+                filterEntity.temp = [value1, value2]
+            elif key == 'time':
+                print("\nНапишите дополнительное значение для фильтрации")
+                value1 = int(input())
+                value2 = int(input())
+                filterEntity.time = [value1, value2]
+            elif key == 'price':
+                value = input()
+                filterEntity.price = value
+            elif key == 'taste':
+                value = input()
+                filterEntity.taste = value
+            else:
+                print("\nприколист")
+            return 0
+        elif menu_entry_index == 3:
+            result = get_recomendation(likes, dislikes, df)
+            result = do_fliter(result, filterEntity)
+            print(result['Название'])
+            print('\n')
+        elif menu_entry_index == 4:
+            return 1
+    except:
+        print("\nОшибочка")
+        return 0
+
+
+def main():
+    likes = []
+    dislikes = []
+    filterEntity = FilterEntity()
+    while True:
+        rc = main_loop(likes, dislikes, filterEntity)
+        if rc == 1:
+            return 0
+
+
+if __name__ == "__main__":
+    main()
